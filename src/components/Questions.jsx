934a503f4e54c08881f5ac4a6226e70c0eafd0de
rasha-v123 <ragibshahriar2022@gmail.com>
2024-08-20 17:24:24 +0600

    the last change

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,69 +1,130 @@
-import React, {useEffect, useState} from "react"
-import SingleQuestion from "./SingleQuestion"
+import React, { useEffect, useState } from "react";
+import SingleQuestion from "./SingleQuestion";
 
 export default function Questions() {
-    
-    const [questions, setQuestions] = useState([])
-    const [questionsAndAnswers, setQuestionsAndAnswers] = useState([])
+  const [questions, setQuestions] = useState([]);
+  const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
+  const [showMessage, setShowMessage] = useState(false);
+  const [numCorrectAnswers, setNumCorrectAnswers] = useState(0);
+  const [showResult, setShowResult] = useState(false);
 
-    useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=10")
-            .then((response) => response.json())
-            .then(data => {
-                setQuestions(data.results)
-                 
-                // setQuestionsAndAnswers(data.results.map((questionObject) => (
-                    
-                // )))
-            
-               setQuestionsAndAnswers(
-                    data.results.map((questionObject) => {
-                        return {
-                            question: questionObject.question,
-                            shuffledAnswers: shuffle([
-                                ...questionObject.incorrect_answers,
-                                questionObject.correct_answer
-                            ]),
-                            correctAnswer: questionObject.correct_answer,
-                            selectedAnswer: "",  
-                        }
-                    })
-                    ) 
-                })
-            }, [])
-            
-            function shuffle(array) {
-                let currentIndex = array.length,
-                randomIndex;
-                
-                while (currentIndex != 0) {
-                    randomIndex = Math.floor(Math.random() * currentIndex);
-                    currentIndex--;
-                    
-                    [array[currentIndex], array[randomIndex]] = [
-                        array[randomIndex],
-                        array[currentIndex]
-                    ]
-                }
-                return array;
-            }
-            
-            console.log(questionsAndAnswers)
+  useEffect(() => {
+    if (questions.length === 0) {
+      fetch("https://opentdb.com/api.php?amount=10")
+        .then((response) => response.json())
+        .then((data) => {
+          setQuestions(data.results);
 
-            return(
-        <>
-            <h1>This is the questions page!</h1>
-            <div className="questions-container">
-                {
-                    questionsAndAnswers.map((questionObject, index) => (
-                        <SingleQuestion key={index} 
-                            question={questionObject.question} 
-                            allAnswers={questionObject.shuffledAnswers}
-                        
-                        />
-                    ))
-                }
-            </div>
-        </>
-    )
+          // setQuestionsAndAnswers(data.results.map((questionObject) => (
+
+          // )))
+
+          setQuestionsAndAnswers(
+            data.results.map((questionObject) => {
+              return {
+                question: questionObject.question,
+                shuffledAnswers: shuffle([
+                  ...questionObject.incorrect_answers,
+                  questionObject.correct_answer,
+                ]),
+                correctAnswer: questionObject.correct_answer,
+                selectedAnswer: "",
+              };
+            })
+          );
+        });
+    }
+  }, [questions]);
+
+  function updateQuestion(currentQuestion, answer) {
+    // console.log(currentQuestion, answer)
+    setQuestionsAndAnswers((prev) =>
+      prev.map((questionObject) =>
+        questionObject.question === currentQuestion
+          ? { ...questionObject, selectedAnswer: answer }
+          : questionObject
+      )
+    );
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    return array;
+  }
+
+  // console.log(questionsAndAnswers)
+
+  function checkAnswer() {
+    const notAllAnswered = questionsAndAnswers.some(
+      (questionObject) => questionObject.selectedAnswer === ""
+    );
+    setShowMessage(notAllAnswered);
+
+    if (!notAllAnswered) {
+      questionsAndAnswers.forEach((questionObject) => {
+        if (questionObject.selectedAnswer === questionObject.correctAnswer) {
+          setNumCorrectAnswers((prevCount) => prevCount + 1);
+        }
+      });
+    }
+
+    setShowResult(true);
+  }
+
+  function playAgain() {
+    setQuestions([]);
+    setQuestionsAndAnswers([]);
+    setShowResult(false);
+    setNumCorrectAnswers(0);
+  }
+
+  console.log(numCorrectAnswers);
+
+  return (
+    <>
+      <h1>This is the questions page!</h1>
+      <div className="questions-container">
+        {questionsAndAnswers.map((questionObject, index) => (
+          <SingleQuestion
+            key={index}
+            question={questionObject.question}
+            allAnswers={questionObject.shuffledAnswers}
+            updateQuestion={updateQuestion}
+            selectedAnswer={questionObject.selectedAnswer}
+            showResult={showResult}
+            correctAnswer={questionObject.correctAnswer}
+          />
+        ))}
+      </div>
+      <div className="text-center">
+        {showMessage ? (
+          <p className="warning-message">Not all questions are answered yet!</p>
+        ) : null}
+        {questions.length > 0 && !showResult ? (
+          <button className="check-btn" onClick={() => checkAnswer()}>
+            Check Answer
+          </button>
+        ) : null}
+      </div>
+      {showResult ? (
+        <div className="text-center">
+          <div>You have scored {numCorrectAnswers}/10.</div>
+          <button className="play-again-btn" onClick={() => playAgain()}>
+            Play again
+          </button>
+        </div>
+      ) : null}
+    </>
+  );
 }
--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,20 +1,49 @@
-import {decode} from "html-entities"
+import { decode } from "html-entities";
 
-export default function(props) {
+export default function (props) {
+  function selectedAnswer(currentQuestion, answer) {
+    props.updateQuestion(currentQuestion, answer);
+  }
 
-    console.log(props)
-    return(
-        <div className="question-container">
-            <h1 className='question'>
-                {props.question}
-            </h1>
-            <div className='answers-btn-container'>
-                {props.allAnswers.map((answer, i) => (
-                    <button className='answer-btn' key={i} onClick={() => {}}>
-                        {decode(answer)}
-                    </button>
-                ))}
-            </div>
-        </div>
-    )
+  console.log(props);
+  console.log(props.correctAnswer);
+
+  return (
+    <div className="question-container">
+      <h1 className="question">{props.question}</h1>
+      <div className="answers-btn-container">
+        {props.allAnswers.map((answer, i) => (
+          <button
+            className={`answer-btn ${
+              answer === props.selectedAnswer ? "selected" : ""
+            } 
+                        ${
+                          props.showResult && answer === props.correctAnswer
+                            ? "correct"
+                            : ""
+                        }
+                        ${
+                          props.showResult &&
+                          answer === props.selectedAnswer &&
+                          answer !== props.correctAnswer
+                            ? "incorrect"
+                            : ""
+                        }
+
+                        ${
+                          props.showResult && answer !== props.correctAnswer
+                            ? "dimmed"
+                            : ""
+                        }
+
+                        `}
+            key={i}
+            onClick={() => selectedAnswer(props.question, answer)}
+          >
+            {decode(answer)}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
